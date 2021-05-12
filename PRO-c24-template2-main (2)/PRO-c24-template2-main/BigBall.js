c0340adb4144b1ab4312d49d11d8c8719fb96433
AdwaithSingh <82128776+AdwaithSingh@users.noreply.github.com>
2021-05-11 21:49:50 -0400

    Add files via upload

---
+++ PRO-c24-template2-main (2)/PRO-c24-template2-main/BigBall.js
@@ -0,0 +1,31 @@
+class BigBall{
+	constructor(x,y,r)
+	{
+		var options={
+			restitution:0.3,
+			friction:5,
+			density:5
+		}
+	// assign options to the rubber ball
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, (50-20)/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			var rubberpos=this.body.position;		
+			push()
+			translate(rubberpos.x, rubberpos.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			stroke("black");
+			fill("green");
+			//draw the ellipse here to display the rubber ball
+			ellipse(0,0,50,50);
+			pop()
+	}
+
+}
